38c5d7b1fefe9799bbdddf1e5ac59ad95bbd5eb2
markelliott-git <melliott1224@gmail.com>
2025-12-04 15:38:49 -0500

    Completed Day 3

--- day_3/part_2.py
+++ day_3/part_2.py
@@ -1,37 +1,39 @@
 import pandas
 
-def find_first_num(bank_list: list):
-    truncated_bank = bank_list[:-1] 
+def find_num(bank: list, battery: int):
 
+    if battery == 1:
+
+        truncated_bank = bank
+
+    else: 
+        truncated_bank = bank[:-battery+1]
+    
     for digit in range(9, -1, -1):
         for idx, num in enumerate(truncated_bank):
             if digit == int(num):
+                truncated_bank = bank[idx+1:]
 
-                return idx, num
-
-def find_second_num(bank_list: list, first_idx):
-    truncated_bank = bank_list[first_idx+1:]
-
-    for digit in range(9, -1, -1):
-        for idx, num in enumerate(truncated_bank):
-            if digit == int(num):
-
-                return idx, num
+                return num, truncated_bank
 
 def main():
 
+    num_batteries = 12
     total_joltage = 0
     input: list = pandas.read_csv('day_3_input.csv')['banks'].to_list()
 
     for bank in input:
         bank_list: list = list(bank)
-        bank_length: int = len(bank_list)
 
-        first_idx, first_num = find_first_num(bank_list)
-        second_idx, second_num = find_second_num(bank_list, first_idx)
+        batteries = []
+        truncated_bank = bank_list
+        for battery in range(num_batteries, 0, -1):
 
+            num, truncated_bank = find_num(truncated_bank, battery) 
 
-        joltage: int = int(first_num + second_num) 
+            batteries.append(num)
+
+        joltage: int = int(''.join(batteries)) 
 
         total_joltage += joltage
 
