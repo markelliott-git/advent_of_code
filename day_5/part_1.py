33815fbf0095522a212d774ade6ceb28374fd8a7
Mark Elliott <mark@Mac.ts.net lan>
2025-12-07 21:08:54 -0500

    Completed Day 5 Part1; Began Day 5 Part 2

--- day_5/part_1.py
+++ day_5/part_1.py
@@ -3,11 +3,23 @@ import pandas
 
 def main():
 
+    fresh_count: int = 0
     ranges: list = pandas.read_csv('day_5_input_ranges.csv')['ranges'].to_list()
     ids: list = pandas.read_csv('day_5_input_ids.csv')['ids'].to_list()
 
-    # print(ranges)
-    print(ids)
+    for id in ids:
+
+        for range in ranges:
+
+            start_range, end_range = range.split('-')
+            start_range = int(start_range)
+            end_range = int(end_range)
+
+            if int(id) >= start_range and int(id) <= end_range:
+                fresh_count += 1
+                break
+
+    print(f'Fresh Items: {fresh_count}')            
 
 if __name__ == "__main__":
     main()
