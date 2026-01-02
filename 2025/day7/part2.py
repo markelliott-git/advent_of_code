f2707ecc08aa8e83abe864b7081252541ef911e8
Mark Elliott <mark@macbook.local>
2026-01-02 13:45:02 -0500

    Completed 2025 day7 part2

--- 2025/day7/part2.py
+++ 2025/day7/part2.py
@@ -1,57 +1,37 @@
 import random
 import time
 
-from rich.live import Live
-from rich.text import Text
 
-    
-# functions
-def generate_grid(grid: list) -> Text:
-    text = Text()
-    
-    for row in grid:
-        text.append(''.join(row) +'\n')
-    return text
+INPUT_FILE: str = '../../advent_of_code_inputs/2025/day7/day_7_input.txt'
+# INPUT_FILE: str = '../../advent_of_code_inputs/2025/day7/day_7_input_test.txt'
 
 
 # Read file
-# INPUT_FILE: str = "day_7_input_test.txt" 
-INPUT_FILE: str = "day_7_input.txt" 
-
 with open(INPUT_FILE) as f:
     grid: list = [list(line.rstrip()) for line in f]
 
-# Render grid
-with Live(generate_grid(grid), refresh_per_second=10) as live:
-    starting_idx: int = grid[0].index('S')
+# Track Timelines
+timeline_tracker: list = [0]*len(grid[0])
 
-    previous_row: list = grid[0]
-    split_count: int = 0
-    for i, row in enumerate(grid[1:]):
-        time.sleep(.1)
+for i, row in enumerate(grid):
 
-        if previous_row == grid[0]:
-            row[starting_idx] = '|'
-            previous_row = row
-            continue
-        else:
-            zipped_rows = zip(previous_row, row)
-            
-            for i, pair in enumerate(zipped_rows):
-                if pair == ('|', '^'):
-                    if row[i-1] == '.' or row[i+1] =='.':
-                        split_count +=1
-                    if row[i-1] == '.':
-                        row[i-1] = '|'
-                        # split_count += 1
-                    if row[i+1] == '.':
-                        row[i+1] = '|'
-                        # split_count += 1
-                elif pair == ('|', '.'):
-                    row[i] = '|'
+    if 'S' in row:
+        s_idx: int = row.index('S')
+        timeline_tracker[s_idx] = 'S'
+        continue
+    else:
 
-        previous_row = row
+        for idx, val in enumerate(row):
+            if timeline_tracker[idx] == 'S':
+                timeline_tracker[idx-1] = 1
+                timeline_tracker[idx+1] = 1
+                timeline_tracker[idx] = 0
+                continue
 
-        live.update(generate_grid(grid))
+            elif val == '^':
+                timeline_tracker[idx-1] += timeline_tracker[idx]
+                timeline_tracker[idx+1] += timeline_tracker[idx]
+                timeline_tracker[idx] = 0
+                
 
-print(f'Total splits: {split_count}')
+print(f'Total timelines: {sum(timeline_tracker)}')
