ed244e54db5b40c591ebe0fd2f47ba8d38bcdd49
Mark Elliott <mark@macbook.local>
2026-01-02 14:01:25 -0500

    Began 2025 day8

--- 2025/day7/part2.py
+++ 2025/day7/part2.py
@@ -1,37 +1,36 @@
-import random
-import time
-
-
 INPUT_FILE: str = '../../advent_of_code_inputs/2025/day7/day_7_input.txt'
 # INPUT_FILE: str = '../../advent_of_code_inputs/2025/day7/day_7_input_test.txt'
 
+def main():
+    # Read file
+    with open(INPUT_FILE) as f:
+        grid: list = [list(line.rstrip()) for line in f]
 
-# Read file
-with open(INPUT_FILE) as f:
-    grid: list = [list(line.rstrip()) for line in f]
+    # Track Timelines
+    timeline_tracker: list = [0]*len(grid[0])
 
-# Track Timelines
-timeline_tracker: list = [0]*len(grid[0])
+    for i, row in enumerate(grid):
 
-for i, row in enumerate(grid):
+        if 'S' in row:
+            s_idx: int = row.index('S')
+            timeline_tracker[s_idx] = 'S'
+            continue
+        else:
 
-    if 'S' in row:
-        s_idx: int = row.index('S')
-        timeline_tracker[s_idx] = 'S'
-        continue
-    else:
+            for idx, val in enumerate(row):
+                if timeline_tracker[idx] == 'S':
+                    timeline_tracker[idx-1] = 1
+                    timeline_tracker[idx+1] = 1
+                    timeline_tracker[idx] = 0
+                    continue
 
-        for idx, val in enumerate(row):
-            if timeline_tracker[idx] == 'S':
-                timeline_tracker[idx-1] = 1
-                timeline_tracker[idx+1] = 1
-                timeline_tracker[idx] = 0
-                continue
+                elif val == '^':
+                    timeline_tracker[idx-1] += timeline_tracker[idx]
+                    timeline_tracker[idx+1] += timeline_tracker[idx]
+                    timeline_tracker[idx] = 0
+                    
 
-            elif val == '^':
-                timeline_tracker[idx-1] += timeline_tracker[idx]
-                timeline_tracker[idx+1] += timeline_tracker[idx]
-                timeline_tracker[idx] = 0
-                
+    print(f'Total timelines: {sum(timeline_tracker)}')
 
-print(f'Total timelines: {sum(timeline_tracker)}')
+if __name__ == "__main__":
+    main()
---
+++ 2025/day8/part1.py
@@ -0,0 +1,3 @@
+
+INPUT_FILE: str = '../../advent_of_code_inputs/2025/day8/day_8_input.txt'
+# INPUT_FILE: str = '../../advent_of_code_inputs/2025/day8/day_8_input_test.txt'
